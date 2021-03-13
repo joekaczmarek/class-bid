9d255d4c2ac4d811584d5667492d07d5bdee22d4
Joe Kaczmarek <joseph.m.kaczmarek@gmail.com>
2021-03-13 17:54:30 -0600

    Sorted bid tracking

--- contracts/Course.sol
+++ contracts/Course.sol
@@ -11,9 +11,16 @@ contract Course {
     uint256 public _minStudents;
     uint256 public _maxStudents;
 
-    uint256 public _bidCount;
-    mapping(address => uint256) public _bids;
+    struct Bid {
+        address bidder;
+        uint256 amount;
+    }
+
+    Bid[] public _sortedBids;
     mapping(address => uint256) public _return;
+
+    bool ended;
+    event BiddingEnded();
 
     constructor(
         BidToken bidToken_,
@@ -26,37 +33,101 @@ contract Course {
         _name = name_;
         _minStudents = minStudents_;
         _maxStudents = maxStudents_;
-        _bidCount = 0;
+
+        ended = false;
     }
 
     function bid(uint256 amount) public {
-        uint256 currentAmount = _bids[msg.sender];
+        uint256 currentBidAmount = 0;
+        uint256 bidIndex = getBidIndexForAddress(msg.sender);
+        if (bidIndex != _sortedBids.length) {
+            currentBidAmount = _sortedBids[bidIndex].amount;
+        }
 
-        if (currentAmount > amount) {
-            uint256 diff = currentAmount - amount;
+        if (currentBidAmount > amount) {
+            uint256 diff = currentBidAmount - amount;
             _return[msg.sender] = diff;
-            _bids[msg.sender] = amount;
+            _sortedBids[bidIndex].amount = amount;
+            reSort(bidIndex);
 
-            // revoke bid
+            // retract bid
             if (amount == 0) {
-                _bidCount -= 0;
+                delete _sortedBids[getBidIndexForAddress(msg.sender)];
             }
         } else {
-            uint256 diff = amount - currentAmount;
+            uint256 diff = amount - currentBidAmount;
             uint256 allowance = _bidToken.allowance(msg.sender, address(this));
             require(allowance >= diff, "Insufficient funds allowed");
             _bidToken.transferFrom(msg.sender, address(this), diff);
-            _bids[msg.sender] = amount;
 
-            // new bid
-            if (currentAmount == 0) {
-                _bidCount += 1;
+            if (currentBidAmount == 0) {
+                // new bid
+                _sortedBids.push(Bid({bidder: msg.sender, amount: amount}));
+            } else {
+                // update bid
+                _sortedBids[bidIndex].amount = amount;
+                reSort(bidIndex);
+            }
+        }
+    }
+
+    function getBidIndexForAddress(address bidder)
+        private
+        view
+        returns (uint256)
+    {
+        uint256 i;
+        for (i = 0; i < _sortedBids.length; i++) {
+            if (_sortedBids[i].bidder == bidder) {
+                return i;
+            }
+        }
+        return _sortedBids.length;
+    }
+
+    function reSort(uint256 index) private {
+        Bid memory newBid = _sortedBids[index];
+
+        if (index > 0) {
+            Bid memory prev = _sortedBids[index - 1];
+
+            while (prev.amount < newBid.amount) {
+                _sortedBids[index - 1] = newBid;
+                _sortedBids[index] = prev;
+
+                index -= 1;
+                if (index > 0) {
+                    prev = _sortedBids[index - 1];
+                } else {
+                    break;
+                }
+            }
+        }
+        if (index < _sortedBids.length - 1) {
+            Bid memory next = _sortedBids[index + 1];
+
+            while (next.amount > newBid.amount) {
+                _sortedBids[index + 1] = newBid;
+                _sortedBids[index] = next;
+
+                index += 1;
+                if (index < _sortedBids.length - 1) {
+                    next = _sortedBids[index + 1];
+                } else {
+                    break;
+                }
             }
         }
     }
 
     function getBid() public view returns (uint256) {
-        return _bids[msg.sender];
+        uint256 index = getBidIndexForAddress(msg.sender);
+        if (index == _sortedBids.length) {
+            return 0;
+        } else {
+            assert(_sortedBids[index].bidder == msg.sender);
+            return _sortedBids[index].amount;
+        }
     }
 
     function reclaim() public {
@@ -65,8 +136,20 @@ contract Course {
         _bidToken.transfer(msg.sender, amount);
     }
 
-    function close() public view {
+    function close() public {
         require(msg.sender == _admin, "Account unauthorized to close bidding");
-        // TODO
+        ended = true;
+        emit BiddingEnded();
+    }
+
+    function bidSuccess() public view returns (bool) {
+        require(ended, "Bidding hasn't ended");
+        uint256 index = getBidIndexForAddress(msg.sender);
+
+        if (index != _sortedBids.length && (index + 1) < _maxStudents) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
